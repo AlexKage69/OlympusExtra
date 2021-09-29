bc7d86e9d076fbd31c4583ace51f2a720e8b37f2
Alexandre Demers <alexandre.demers@dfo-mpo.gc.ca>
2021-09-29 11:25:26 -0400

    Fixed Separation of Gods and Goddess. Missing testing for when adding gift to lock Codex God.

--- SeparateGoddess.lua
+++ SeparateGoddess.lua
@@ -1,11 +1,96 @@
 -- Separate Gods from Goddess to add more gods
 
 ModUtil.RegisterMod("SeparateGoddess")
+
+--local mod = "SeparateGoddess"
+--local package = "HeroesIcons"
+
+function MoveGodToSubCodex(category, CodexOrderingIndex, god)
+    for godtoremove, info in pairs(Codex.OlympianGods.Entries) do
+        if(godtoremove == god) then
+            --table.insert(CodexOrdering[category].Order, god)
+            Codex[category].Entries[god] = Codex.OlympianGods.Entries[god]
+            table.remove(CodexOrdering.OlympianGods.Order, CodexOrderingIndex)
+            Codex.OlympianGods.Entries[god] = nil
+            return true
+        end
+    end
+    return false
+end
+
+function AddGodToSubCodex(category, god)
+    --table.insert(CodexOrdering[category].Order, god)
+    Codex[category].Entries[god] = Codex.OlympianGods.Entries[god]
+    return true
+end
+
+function AddNewOlympians()
+    for modName, section in pairs(CodexExtra) do
+        Codex[modName] = ModUtil.Table.Copy(section.Codex)
+        CodexOrdering[modName] = {Order = ModUtil.Table.Copy(section.Order)}
+        local added = false
+        for i, value in ipairs(section.Order) do
+                for j, god in ipairs(CodexOrdering.OlympianGods.Order) do
+                    -- Already exists
+                    if(god == value) then
+                        added = MoveGodToSubCodex(modName, j, god) or added
+                    else -- New God ValueOptions
+                        --added = AddGodToSubCodex(modName, god) or added
+                    end
+                end
+        end    
+        if(added) then
+            table.insert(CodexOrdering.Order, section.Index, modName)
+        end
+    end
+end
+
+OnAnyLoad{ function() 
+    CodexUI.MaxChapters = 4
+    AddNewOlympians()
+end}
+
+ModUtil.WrapBaseFunction( "SetupMap", function(baseFunc)
+    --DebugPrint({Text = "@"..mod.." Trying to load package "..package..".pkg"})
+    --LoadPackages({Name = package})
+    return baseFunc()
+end)
+
+ModUtil.WrapBaseFunction( "HandleUpgradeChoiceSelection", function(baseFunc, screen, button)
+    baseFunc(screen, button)
+	CheckCodexUnlock( "OlympianGoddess", button.UpgradeName )
+end)
+
+ModUtil.WrapBaseFunction( "AttemptGift", function(baseFunc, CurrentRun, target)
+    baseFunc(CurrentRun, target)
+	local name = GetGenusName( target )
+	local resourceName = GetNextGiftResource( name )
+	local resourceQuantity = GetNextGiftResourceQuantity( name )
+    if PlayingTextLines or not IsInputAllowed({ }) or target == nil or not HasResource( resourceName, resourceQuantity ) then
+        return
+    end
+    if CanReceiveGift( target ) then
+        CheckCodexUnlock( "OlympianGoddess", name )
+    end
+end)
+
+ModUtil.WrapBaseFunction( "OpenCodexScreen", function(baseFunc)
+    if(CodexStatus) then
+        ModUtil.Hades.PrintStackChunks("------Check Codex Status-----")    
+        ModUtil.Hades.PrintStackChunks(ModUtil.ToString.Deep(CodexStatus.OlympianGods))
+        ModUtil.Hades.PrintStackChunks("-----------")    
+        ModUtil.Hades.PrintStackChunks(ModUtil.ToString.Deep(CodexStatus.OlympianGoddess))
+    end
+    baseFunc()
+end)
+
+-- Run OnLoad
+
 --New Data
-CodexOrderingExtra = {
+CodexExtra = {
 	OlympianGoddess = {
 		Index = 3,
-		Order = { "AthenaUpgrade", "AphroditeUpgrade", "ArtemisUpgrade" },
+		Order = { "AthenaUpgrade", "AphroditeUpgrade", "ArtemisUpgrade", "DemeterUpgrade" },
 		Codex = {
 			UnlockType = CodexUnlockTypes.Boon,
 			TitleText = "Codex_OlympianGoddessChapter",
@@ -13,44 +98,3 @@ CodexOrderingExtra = {
 		}
     }
 }
-
-OnAnyLoad{ function()
-    CodexUI.MaxChapters = 3
-    for key, section in pairs(CodexOrderingExtra) do
-        Codex[key] = section.Codex
-        CodexOrdering[key] = { Order = {}}
-        local added = false
-        for i, value in ipairs(section.Order) do
-                for j, god in ipairs(CodexOrdering.OlympianGods.Order) do
-                    if(god == value) then
-                        for godtoremove, godinfo in pairs(Codex.OlympianGods.Entries) do
-                            if(godtoremove == god) then
-                                table.insert(CodexOrdering[key].Order, god)
-                                Codex[key].Entries[god] = Codex.OlympianGods.Entries[value]
-                                table.remove(CodexOrdering.OlympianGods.Order, j)
-                                Codex.OlympianGods.Entries[value] = nil
-                                added = true
-                            end
-                        end
-                    else
-                        Codex[key].Entries[value] = section[value]
-                        added = true
-                    end
-                end
-        end    
-        if(added) then
-            table.insert(CodexOrdering.Order, section.Index, key)
-        end
-    end
-end}
-
-ModUtil.WrapBaseFunction( "OpenCodexScreen", function(baseFunc)
-    baseFunc()
-end)
-
-ModUtil.WrapBaseFunction( "CodexOpenChapter", function(baseFunc,  screen, button, args)
-    --ModUtil.Hades.PrintStackChunks(ModUtil.ToString.Deep(button.ChapterData.Entries.DemeterUpgrade))
-    if(button.ChapterData ~= nil) then
-        baseFunc(screen, button, args)
-    end
-end)
