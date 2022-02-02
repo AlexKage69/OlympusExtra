2bd1ceaaba4c66936219f21e36e48d088ba87d0c
Alexandre Demers <alexandre.demers@dfo-mpo.gc.ca>
2022-02-02 11:48:18 -0500

    Fixed Spawn of Apollo Boon, Apollo Upgrade page, and removed other gods association

--- OlympusExtra.lua
+++ OlympusExtra.lua
@@ -1,9 +1,30 @@
 -- Going to combine all mods
 
-ModUtil.RegisterMod("OlympusExtra")
+--[[ModUtil.RegisterMod("OlympusExtra")
 
-ModUtil.WrapBaseFunction( "OpenCodexScreen", function(baseFunc, args)
-    baseFunc(args)
-    
-    --ModUtil.Hades.PrintStack(CodexOrdering.OlympianGods.Order[1])
+-- Add new LootData and more
+ModUtil.LoadOnce(function ()
+    for lootName, section in pairs(OlympusExtra.LootData) do
+        LootData[lootName] = OlympusExtra.LootData[lootName]
+    end
+    for achivementName, section in pairs(OlympusExtra.AchievementData) do
+        GameData.AchievementData[achivementName] = section
+    end
+    for enemyName, section in pairs(OlympusExtra.EnemiesData) do
+        UnitSetData.Enemies[enemyName] = section
+    end
+    for giftName, section in pairs(OlympusExtra.GiftData) do
+        table.insert(GiftOrdering, 13, section.GiftOrdering);
+        GiftData[giftName] = section.GiftData;
+    end
+    for traitName, section in pairs(OlympusExtra.TraitData) do
+        TraitData[traitName] = section;
+    end
 end)
+
+function AddGodToSubCodex(category, god)
+    table.insert(CodexOrdering[category].Order, god)
+    Codex[category].Entries[god] = OlympusExtra.Codex[category].Codex.Entries[god]
+    table.insert(BoonInfoScreenData.Ordering, god)
+    return true
+end--]]
