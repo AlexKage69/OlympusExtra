be7d4c613fa236f1cf787fb82377c64f6df59eec
Phy Poh <phypoh@gmail.com>
2022-03-11 21:20:37 +0800

    Cleanup
    Removing AssetsLab

--- AssetsLab/packAssets.ps1
+++
@@ -1,4 +0,0 @@
- texture_packing_wheel pk -s PackMe -b OEAssets
- deppth pk -s OEAssets -t OEAssets.pkg 
- Move-Item OEAssets.pkg ../Packages/OEAssets.pkg -Force
- Move-Item OEAssets.pkg_manifest ../Packages/OEAssets.pkg_manifest -Force
