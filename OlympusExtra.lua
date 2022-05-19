ba20b7c63de97c909107a9d238a167ccfb7d91da
Alexandre Demers <alexandre.demers@dfo-mpo.gc.ca>
2022-05-19 11:55:23 -0400

    Fixed Hyacinth Fx. Fixed text. Moved ApolloExtra.lua

--- OlympusExtra.lua
+++ OlympusExtra.lua
@@ -4,34 +4,33 @@ if ModUtil ~= nil then
     if ModUtil.Mods.Data["ApolloExtra"] ~= nil then
         --External mod interaction
         if CodexMenuData ~= nil then
-        local OlympusCodexMenuData = ModUtil.Entangled.ModData(CodexMenuData)
-        OlympusCodexMenuData.ApolloUpgrade = {
-        "ApolloWeaponTrait", "ApolloDashTrait", "ApolloRangedTrait", "ApolloSecondaryTrait", 
-        "ApolloShoutTrait", "ApolloRetaliateTrait", "FountainDefenseTrait", "FountainCoinTrait",
-        "ApolloBlindedTrait", "ApolloHealTrait", "RerollObolTrait", "RerollBoonTrait",
-        "MissChanceTrait",
-        }
-        table.insert(OlympusCodexMenuData.Duos, "FamedDuetTrait")
-        table.insert(OlympusCodexMenuData.Duos, "WarSongTrait")
-        table.insert(OlympusCodexMenuData.Duos, "HyacinthTrait")
-        table.insert(OlympusCodexMenuData.Duos, "BlindDurationTrait")
-        local OlympusCodexBoonTable = ModUtil.Entangled.ModData(CodexBoonTable)
-        table.insert(OlympusCodexBoonTable, "ApolloUpgrade")
+            local OlympusCodexMenuData = ModUtil.Entangled.ModData(CodexMenuData)
+            OlympusCodexMenuData.ApolloUpgrade = {
+            "ApolloWeaponTrait", "ApolloDashTrait", "ApolloRangedTrait", "ApolloSecondaryTrait", 
+            "ApolloShoutTrait", "ApolloRetaliateTrait", "FountainDefenseTrait", "FountainCoinTrait",
+            "ApolloBlindedTrait", "ApolloHealTrait", "RerollObolTrait", "RerollBoonTrait",
+            "MissChanceTrait",
+            }
+            table.insert(OlympusCodexMenuData.Duos, "FamedDuetTrait")
+            table.insert(OlympusCodexMenuData.Duos, "WarSongTrait")
+            table.insert(OlympusCodexMenuData.Duos, "HyacinthTrait")
+            table.insert(OlympusCodexMenuData.Duos, "BlindDurationTrait")
+            local OlympusCodexBoonTable = ModUtil.Entangled.ModData(CodexBoonTable)
+            table.insert(OlympusCodexBoonTable, "ApolloUpgrade")
 
-        table.insert(OlympusCodexMenuData.BeowulfTraits, "ShieldLoadAmmo_ApolloRangedTrait")
+            table.insert(OlympusCodexMenuData.BeowulfTraits, "ShieldLoadAmmo_ApolloRangedTrait")
 
-        local OlympusRealGodNames = ModUtil.Entangled.ModData(RealGodNames)
-        table.insert(OlympusRealGodNames, "Apollo")
+            local OlympusRealGodNames = ModUtil.Entangled.ModData(RealGodNames)
+            table.insert(OlympusRealGodNames, "Apollo")
 
-        --[[ModUtil.WrapBaseFunction( "SetupMap", function(baseFunc)
-            LoadPackages({Names = {
-                "ApolloUpgrade",
-            }})
-            return baseFunc()
-        end)]]
+            --[[ModUtil.WrapBaseFunction( "SetupMap", function(baseFunc)
+                LoadPackages({Names = {
+                    "ApolloUpgrade",
+                }})
+                return baseFunc()
+            end)]]
         end            
     end
-
     -- Recompile data. Required for each gods so generic here.
     ResetKeywords()
     SetupRunData()
