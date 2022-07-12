d99eff5545ed9992ce56c607aaf92d58acd17ddb
Phy Poh <phypoh@gmail.com>
2022-07-12 21:58:31 +0800

    Changed packaging format

--- AssetsLab/packAssets.ps1
+++ AssetsLab/packAssets.ps1
@@ -1,4 +1,12 @@
- texture_packing_wheel pk -s PackMe -b OEAssets
- deppth pk -s OEAssets -t OEAssets.pkg 
- Move-Item OEAssets.pkg ../Packages/OEAssets.pkg -Force
- Move-Item OEAssets.pkg_manifest ../Packages/OEAssets.pkg_manifest -Force
+texture_packing_wheel pk -s PackMe/Temp -b OETemp
+deppth pk -s OETemp -t OETemp.pkg
+
+texture_packing_wheel pk -s PackMe/Apollo -b OEApollo
+deppth pk -s OEApollo -t OEApollo.pkg 
+
+texture_packing_wheel pk -s PackMe/Hestia -b OEHestia
+deppth pk -s OEHestia -t OEHestia.pkg
+
+Remove-Item 'OEApollo' -Recurse
+Remove-Item 'OEHestia' -Recurse
+Remove-Item 'OETemp' -Recurse
