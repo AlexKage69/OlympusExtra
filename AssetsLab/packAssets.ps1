9db7acb04cc04ca237a6740b4cdc02348bad1d01
Phy Poh <phypoh@gmail.com>
2022-07-27 13:01:38 +0800

    Hestia assets

--- AssetsLab/packAssets.ps1
+++ AssetsLab/packAssets.ps1
@@ -1,12 +1,9 @@
-texture_packing_wheel pk -s PackMe/Temp -b OETemp
-deppth pk -s OETemp -t OETemp.pkg
-
 texture_packing_wheel pk -s PackMe/Apollo -b OEApollo
-deppth pk -s OEApollo -t OEApollo.pkg 
+deppth pk -s OEApollo -t OEApollo.pkg
+
+Remove-Item 'OEApollo' -Recurse
 
 texture_packing_wheel pk -s PackMe/Hestia -b OEHestia
 deppth pk -s OEHestia -t OEHestia.pkg
 
-Remove-Item 'OEApollo' -Recurse
 Remove-Item 'OEHestia' -Recurse
-Remove-Item 'OETemp' -Recurse
