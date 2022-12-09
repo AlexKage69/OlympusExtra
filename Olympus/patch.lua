a4728804b39751ffe94f5c9fdb27053cbab5bfab
AlexKage <alexandre69.demers@gmail.com>
2022-12-09 12:54:11 -0500

    Fixed KeepsakeFails on Load

--- Olympus/patch.lua
+++ Olympus/patch.lua
@@ -9,6 +9,31 @@ ModUtil.Path.Wrap( "Heal",
     end
 )
 
+ModUtil.Path.Wrap( "GetKeepsakeLevel", 
+	function(baseFunc, traitName )
+        if TraitData[traitName] == nil then
+            return 1
+        else
+            return baseFunc(traitName)
+        end
+    end
+)
+ModUtil.Path.Wrap( "EquipKeepsake", 
+	function(baseFunc, heroUnit, traitName, args )
+        if traitName == nil or TraitData[traitName] == nil then
+            return
+        end
+        baseFunc( heroUnit, traitName, args)
+    end
+)
+ModUtil.Path.Wrap( "EquipAssist", 
+	function(baseFunc,  heroUnit, traitName, args )
+        if traitName == nil or TraitData[traitName] == nil then
+            return
+        end
+        baseFunc( heroUnit, traitName, args)
+    end
+)
 --[[ModUtil.Path.Wrap( "AddRerolls", 
 	function(baseFunc, amount, source, args )
         if type(amount) == "number" then
